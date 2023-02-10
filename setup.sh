2593574260eba685593500ef27b376d696a8fe06
andrew153d <109369025+andrew153d@users.noreply.github.com>
2023-02-09 23:29:39 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,45 @@ sudo apt-get install bladerf-fpga-hostedxa4
 
 sudo apt-get install gnuradio
 
+sudo apt-get install -y libusb-1.0-0-dev libusb-1.0-0 git cmake g++ \
+   libboost-all-dev libgmp-dev swig python3-numpy python3-matplotlib \
+   python3-mako python3-sphinx python3-lxml doxygen libfftw3-dev \
+   libsdl1.2-dev libgsl-dev libqwt-qt5-dev libqt5opengl5-dev python3-pyqt5 \
+   liblog4cpp5-dev libzmq3-dev python3-yaml python3-click python3-click-plugins \
+   python3-zmq python3-scipy python3-gi python3-gi-cairo gir1.2-gtk-3.0 \
+   libcodec2-dev libgsm1-dev libqt5svg5-dev libpulse-dev pulseaudio alsa-base \
+   libasound2 libasound2-dev pybind11-dev libsndfile-dev
+   
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+gnuradio-companion
+
+mkdir ~/gr
+
+cd ~/gr
+git clone git://git.osmocom.org/gr-iqbal
+cd gr-iqbal
+git submodule update --init --recursive
+mkdir build && cd build
+cmake ..
+make -j$(nproc) 
+sudo make install && sudo ldconfig
+
+cd ~/gr
+git clone https://github.com/Nuand/gr-osmosdr -b dev-gr-3.9
+cd gr-osmosdr
+mkdir build && cd build
+cmake ..
+make -j$(nproc)
+sudo make install && sudo ldconfig
+
+cd ~/gr
+git clone https://github.com/Nuand/gr-bladeRF.git
+cd gr-bladeRF
+mkdir build
+cd build
+cmake ..
+make -j4
+sudo make install
 
 #!/bin/bash
 #
