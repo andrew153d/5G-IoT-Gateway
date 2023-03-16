ea4ac1b8912080224e04ab9e1795653bb49243ec
andrew153d <109369025+andrew153d@users.noreply.github.com>
2023-03-16 17:47:21 -0400

    renamed Repo

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@
 # Install SD card, plug in, and follow prompts to start up the Pi
 # Open the command line and enter the following
 # sudo apt-get install git
-# git clone https://github.com/andrew153d/Rpi-bladeRF-GNUradio.git
-# cd Rpi-bladeRF-GNUradio
+# git clone https://github.com/andrew153d/5G-IoT-Gateway.git
+# cd 5G-IoT-Gateway
 # chmod +x setup.sh
 # ./setup.sh
 # You will have to hit enter and say yes to a couple prompts
