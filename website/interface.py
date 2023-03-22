5124d7c411ce2e91ba9dc8dec56a507e3b93ae01
andrew153d <andrewwisnergrow@gmail.com>
2023-03-22 08:38:41 -0400

    simple AJAX request working

--- website/interface.py
+++ website/interface.py
@@ -1,9 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+@app.route('/info/networkStats', methods = ['POST'])
+def index2():
+    try:
+        genre = request.get_json()['title']
+        print(genre)
+    except:
+        print("fail")
+    finally:
+        return jsonify({'title':4, 'id':5})
+
 @app.route('/')
 def index():
+    print("index")
     return render_template('index.html')
 
 if __name__ == "__main__":
--- website/static/script.js
+++ website/static/script.js
@@ -1,6 +1,37 @@
 
 
-function doSomething() {
-    alert('do something');
-    console.log('do something ')
+function getNetwork() {
+    color = document.getElementById("networkStats").style.backgroundColor;
+    document.getElementById("networkStats").style.backgroundColor = "#454040";
+    const response = fetch('/info/networkStats', {
+
+        method: 'POST',
+
+        body: JSON.stringify({
+
+            'title': 10,
+
+            'description': 1,
+
+            'yor': 2,
+
+            'publisher': 3,
+
+            'genre': 4
+
+        }),
+
+        headers: {
+            'Content-Type': 'application/json',
+        }
+
+    })
+        .then(response => response.json())
+
+        .then(jsonResponse => {
+
+            document.getElementById("networkStats").style.backgroundColor = color;
+
+        })
+
 }
