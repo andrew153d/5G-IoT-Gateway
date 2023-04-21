12bcf86b759cba008b31affa549f57232629ec13
andrew153d <109369025+andrew153d@users.noreply.github.com>
2023-04-21 09:55:31 -0400

    replace apt-get with apt

--- setup.sh
+++ setup.sh
@@ -18,13 +18,13 @@ systemctl status x11vnc.service
 # -------- --------
 
 
-sudo apt-get update && sudo apt-get upgrade -y
+sudo apt update && sudo apt upgrade -y
 
 sudo apt install ssh -y
 
 # -------- install bladerf images--------
 sudo add-apt-repository -y ppa:nuandllc/bladerf
-sudo apt-get update
+sudo apt update
 
 mkdir ~/blade
 sudo wget -P ~/blade/ https://www.nuand.com/fx3/bladeRF_fw_latest.img
@@ -32,7 +32,7 @@ sudo wget -P ~/blade/ https://www.nuand.com/fpga/hostedxA4-latest.rbf
 # -------- --------
 
 # -------- install GNU-radio and dependencies --------
-sudo apt-get install -y libusb-1.0-0-dev libusb-1.0-0 git cmake g++ \
+sudo apt install -y libusb-1.0-0-dev libusb-1.0-0 git cmake g++ \
    libboost-all-dev libgmp-dev swig python3-numpy python3-matplotlib \
    python3-mako python3-sphinx python3-lxml doxygen libfftw3-dev \
    libsdl1.2-dev libgsl-dev libqwt-qt5-dev libqt5opengl5-dev python3-pyqt5 \
@@ -49,12 +49,11 @@ export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 # -------- --------
 
 # -------- install bladerf(doesn't seem to install python bindings)--------
-sudo apt-get install -y bladerf libbladerf-dev bladerf-firmware-fx3 -y
-sudo apt-get install bladerf-fpga-hostedxa4 -y   
+sudo apt install -y bladerf libbladerf-dev bladerf-firmware-fx3 -y
+sudo apt install bladerf-fpga-hostedxa4 -y   
 # -------- --------
 
 # -------- install GNU-radio blocks--------
-mkdir ~/gr
 
 cd ~/gr
 git clone git://git.osmocom.org/gr-iqbal
@@ -114,7 +113,7 @@ if [ "$Modem" = "Waveshare" ]; then
    cd ~/
    wget -P ~/ https://www.waveshare.com/w/upload/1/1e/SIM820X_RNDIS.zip
    sudo pip3 install pyserial -y
-   sudo apt-get install unzip -y
+   sudo apt install unzip -y
    unzip  SIM820X_RNDIS.zip
    sudo chmod 777 SIM820X_RNDIS.py
    sudo python3 SIM820X_RNDIS.py
@@ -134,7 +133,7 @@ fi
 # -------- minicom: used for connection with bladerf
 # net-tools: give access to ifconfig
 # speedtest-cli: network speed tool --------
-sudo apt-get install minicom net-tools speedtest-cli -y
+sudo apt install minicom net-tools speedtest-cli -y
 # -------- --------
 
 # -------- tools for web app --------
